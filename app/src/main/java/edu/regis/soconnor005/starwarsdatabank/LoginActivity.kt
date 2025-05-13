2e51739c657cb56095977cc8d508d853d7490623
Sean O'Connor <gh@seano.dev>
2025-05-13 16:46:22 -0600

    Update splash screen

--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/LoginActivity.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/LoginActivity.kt
@@ -1,16 +1,34 @@
 package edu.regis.soconnor005.starwarsdatabank
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
+        // Fun animation when the splash screen is exiting
+        splashScreen.setOnExitAnimationListener { view ->
+            val slideUp = ObjectAnimator.ofFloat(view, View.SCALE_X, 1f, 0f)
+            slideUp.interpolator = AnticipateInterpolator()
+            slideUp.duration = 200L
+            slideUp.doOnEnd { view.remove() }
+            slideUp.start()
+        }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_login)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
