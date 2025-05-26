fd743fe11ebc1df47e9ef835889020d30952b416
Sean O'Connor <gh@seano.dev>
2025-05-26 16:41:20 -0600

    Create EntryCategory enum

--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/data/Entry.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/data/Entry.kt
@@ -2,4 +2,5 @@ package edu.regis.soconnor005.starwarsdatabank.data
 
 import java.io.Serializable
 
-data class Entry(val category: String, val name: String, val description: String) : Serializable
+data class Entry(val category: EntryCategory, val name: String, val description: String) :
+    Serializable
