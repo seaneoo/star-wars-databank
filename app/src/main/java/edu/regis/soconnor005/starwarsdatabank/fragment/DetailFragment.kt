750e46f273a7b7f9a6bc1ed0fc58a268da460b1f
Sean O'Connor <gh@seano.dev>
2025-05-25 19:24:56 -0600

    Create item screens

--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/DetailFragment.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/DetailFragment.kt
@@ -4,55 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import edu.regis.soconnor005.starwarsdatabank.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private val args by navArgs<DetailFragmentArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_detail, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) = DetailFragment().apply {
-            arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
-            }
+        val itemName = args.itemName
+        view.findViewById<TextView>(R.id.item_name).text = itemName
+
+        view.findViewById<ImageButton>(R.id.button_back).setOnClickListener {
+            findNavController().navigate(DetailFragmentDirections.actionDetailFragmentToListFragment())
         }
+
+        val buttonEdit = view.findViewById<ImageButton>(R.id.button_edit)
+        buttonEdit.contentDescription = getString(R.string.edit_item, itemName)
+        buttonEdit.setOnClickListener {
+            findNavController().navigate(
+                DetailFragmentDirections.actionDetailFragmentToEditFragment(
+                    itemName
+                )
+            )
+        }
+
+        return view
     }
 }
--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/ListFragment.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/ListFragment.kt
@@ -4,55 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import edu.regis.soconnor005.starwarsdatabank.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_list, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) = ListFragment().apply {
-            arguments = Bundle().apply {
-                putString(ARG_PARAM1, param1)
-                putString(ARG_PARAM2, param2)
-            }
+        val itemList = view.findViewById<ListView>(R.id.item_list)
+        val items = resources.getStringArray(R.array.items)
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_item, items)
+
+        val navController = findNavController()
+
+        itemList.adapter = arrayAdapter
+        itemList.setOnItemClickListener { parent, view, position, id ->
+            val item = items[position]
+            val action = ListFragmentDirections.actionListFragmentToDetailFragment(item)
+            navController.navigate(action)
         }
+
+        view.findViewById<Button>(R.id.button_add_item).setOnClickListener {
+            navController.navigate(ListFragmentDirections.actionListFragmentToAddFragment())
+        }
+
+        return view
     }
 }
