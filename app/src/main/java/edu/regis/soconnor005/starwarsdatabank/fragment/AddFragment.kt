0bc00aa1a4f7cb9099e97bdc9ee3980156d4ffa5
Sean O'Connor <gh@seano.dev>
2025-05-29 16:05:51 -0600

    Complete logic to select category

--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/AddFragment.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/AddFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import edu.regis.soconnor005.starwarsdatabank.R
+import edu.regis.soconnor005.starwarsdatabank.data.EntryCategory
 
 class AddFragment : Fragment() {
     override fun onCreateView(
@@ -20,8 +23,16 @@ class AddFragment : Fragment() {
             findNavController().navigate(AddFragmentDirections.actionAddFragmentToListFragment())
         }
 
+        val autoComplete = view.findViewById<AutoCompleteTextView>(R.id.dropdown_category)
+        val categories = EntryCategory.entries.map { it.name }
+        val arrayAdapter = ArrayAdapter(requireContext(), R.layout.list_item, categories)
+        autoComplete.setText(categories[0])
+        autoComplete.setAdapter(arrayAdapter)
+
         view.findViewById<Button>(R.id.button_add_item).setOnClickListener {
-            findNavController().navigate(AddFragmentDirections.actionAddFragmentToListFragment())
+//            findNavController().navigate(AddFragmentDirections.actionAddFragmentToListFragment())
+            val entryCategory = EntryCategory.valueOf(autoComplete.text.toString())
+            // TODO actually add the item
         }
 
         return view
