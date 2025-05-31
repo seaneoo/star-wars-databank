5ba44f5350efdcfa536e711d820286dbcbb99f60
Sean O'Connor <gh@seano.dev>
2025-05-31 14:31:06 -0600

    Add error feedback to add screen

--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/AddFragment.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/fragment/AddFragment.kt
@@ -10,11 +10,11 @@ import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
+import edu.regis.soconnor005.starwarsdatabank.R
 import edu.regis.soconnor005.starwarsdatabank.data.DatabankViewModel
 import edu.regis.soconnor005.starwarsdatabank.data.Entry
 import edu.regis.soconnor005.starwarsdatabank.data.EntryCategory
 import edu.regis.soconnor005.starwarsdatabank.databinding.FragmentAddBinding
-import java.util.InputMismatchException
 
 class AddFragment : Fragment() {
     private var _binding: FragmentAddBinding? = null
@@ -48,10 +48,11 @@ class AddFragment : Fragment() {
 
         // Add logic to Add button
         binding.buttonAddItem.setOnClickListener {
-            addEntry(buildNewEntry(binding.category, binding.name, binding.description))
-            findNavController().navigate(
-                AddFragmentDirections.actionAddFragmentToListFragment()
-            )
+            if (addEntry(buildNewEntry(binding.category, binding.name, binding.description))) {
+                findNavController().navigate(
+                    AddFragmentDirections.actionAddFragmentToListFragment()
+                )
+            }
         }
     }
 
@@ -59,28 +60,48 @@ class AddFragment : Fragment() {
         categoryInput: AutoCompleteTextView?,
         nameInput: EditText?,
         descriptionInput: EditText?,
-    ): Entry {
+    ): Entry? {
+        var error = false
+
         val category = if (categoryInput != null && !categoryInput.text.toString().isBlank()) {
             EntryCategory.valueOf(categoryInput.text.toString())
         } else {
-            throw InputMismatchException("category is required")
+            error = true
+            displayError(getString(R.string.category_required))
+            null
         }
         val name = if (nameInput != null && !nameInput.text.toString().isBlank()) {
             nameInput.text.toString().trim()
         } else {
-            throw InputMismatchException("name is required")
+            error = true
+            displayError(getString(R.string.name_required))
+            null
         }
         val description =
             if (descriptionInput != null && !descriptionInput.text.toString().isBlank()) {
                 descriptionInput.text.toString().trim()
             } else {
-                throw InputMismatchException("description is required")
+                error = true
+                displayError(getString(R.string.description_required))
+                null
             }
-        val entry = Entry(category = category, name = name, description = description)
-        return entry
+
+        return if (!error) {
+            Entry(category = category!!, name = name!!, description = description!!)
+        } else null
     }
 
-    private fun addEntry(entry: Entry) {
-        viewModel.addEntry(entry)
+    private fun displayError(message: String) {
+        binding.errorMessage?.let {
+            it.visibility = View.VISIBLE
+            it.text = getString(R.string.error, message)
+        }
+    }
+
+    private fun addEntry(entry: Entry?): Boolean {
+        return if (entry != null) {
+            viewModel.addEntry(entry)
+            true
+        } else false
     }
 }
