84088dfca31f4d967b3303208cd2f732744c18bf
Sean O'Connor <gh@seano.dev>
2025-06-02 15:27:01 -0600

    Bump version name, code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "edu.regis.soconnor005.starwarsdatabank"
         minSdk = 31
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2025.06.08"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
