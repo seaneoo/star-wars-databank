3be0d25be124dec6cb5cfecfdb8d5618d10b982b
Sean O'Connor <gh@seano.dev>
2025-06-09 09:42:28 -0600

    Add version catalog entry for androidx.navigation.safeargs.kotlin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("androidx.navigation.safeargs.kotlin")
+    alias(libs.plugins.navigation)
     alias(libs.plugins.kapt)
 }
 
