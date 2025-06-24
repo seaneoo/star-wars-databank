ceb96763eeaae34a45dc81383d83a753bb18dae4
Sean O'Connor <gh@seano.dev>
2025-06-24 10:07:03 -0600

    Remove unused code

--- app/src/main/java/edu/regis/soconnor005/starwarsdatabank/auth/AuthService.kt
+++ app/src/main/java/edu/regis/soconnor005/starwarsdatabank/auth/AuthService.kt
@@ -1,31 +1,10 @@
 package edu.regis.soconnor005.starwarsdatabank.auth
 
 import com.google.firebase.Firebase
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.auth
-import kotlinx.coroutines.channels.awaitClose
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.tasks.await
 
 object AuthService {
-    val currentUser: Flow<FirebaseUser?>
-        get() = callbackFlow {
-            val listener = FirebaseAuth.AuthStateListener { auth ->
-                trySend(auth.currentUser)
-            }
-            Firebase.auth.addAuthStateListener(listener)
-            awaitClose { Firebase.auth.removeAuthStateListener(listener) }
-        }
-
-    val currentUserId: String
-        get() = Firebase.auth.currentUser?.uid.orEmpty()
-
-    fun hasUser(): Boolean {
-        return Firebase.auth.currentUser != null
-    }
-
     suspend fun createAccount(email: String, password: String) {
         Firebase.auth.createUserWithEmailAndPassword(email, password).await()
     }
